3c48a0548c1eb1c285156134b245789604bad027
Santiard <jesardila17@gmail.com>
2025-09-22 15:21:39 -0500

    first commit

---
+++ src/main/java/com/casaglass/casaglass_backend/CasaglassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.casaglass.casaglass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaglassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaglassBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casaglass/casaglass_backend/CasaglassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casaglass.casaglass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaglassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
