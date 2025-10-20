2db26d6e63efbea8630656f4b3d44eb5f5ca15a9
Santiard <jesardila17@gmail.com>
2025-10-20 18:26:40 -0500

    verfiicaciones

--- src/main/java/com/casaglass/casaglass_backend/model/TipoProducto.java
+++ src/main/java/com/casaglass/casaglass_backend/model/TipoProducto.java
@@ -1,10 +1,7 @@
 package com.casaglass.casaglass_backend.model;
 
 public enum TipoProducto {
-    MATE,
-    BLANCO,
-    NEGRO,
-    BRONCE,
-    NATURAL,
-    NA // Equivalente a N/A pero válido para Java
+    UNID,
+    PERFIL
+     // Equivalente a N/A pero válido para Java
 }
